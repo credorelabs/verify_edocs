746892a22235e9b726835650d55aa9c9e9dab997
abhilashsethi <abhilashsethi9@gmail.com>
2024-05-29 12:31:53 +0530

    steps added

--- src/components/VerifyPageContent/DropZoneSection.tsx
+++ src/components/VerifyPageContent/DropZoneSection.tsx
@@ -9,6 +9,7 @@ import { useProviderContext } from "../../common/contexts/provider";
 import { ChainId } from "../../constants/chain-info";
 import { DOCSCAN, FILESCAN } from "../Animation";
 import Lottie from "react-lottie";
+import { Info } from "@mui/icons-material";
 
 interface DraggableDemoCertificateProps {
   chainId: ChainId;
@@ -73,8 +74,8 @@ export const DropZoneSectionContainer = (): React.ReactElement => {
   };
 
   return currentChainId ? (
-    <div className="flex mt-4">
-      <div className="w-full">
+    <div className="flex mt-4 gap-8">
+      <div className="w-2/3">
         <div
           id="demoDrop"
           onDrop={(event) => {
@@ -89,14 +90,14 @@ export const DropZoneSectionContainer = (): React.ReactElement => {
           <CertificateDropZoneContainer />
         </div>
       </div>
-      {/* <div className="my-auto mx-auto">
-          <Lottie options={defaultOptions}
-            isPaused={false}
-            isClickToPauseDisabled={true}
-            height={400}
-            width={400}
-          />
-        </div> */}
+      <div className="w-1/3 my-auto mx-auto border-2 border-[#F15928] border-dotted p-1 rounded-lg animate-pulse flex gap-2 justify-center items-center ">
+        <Info />
+        <div>
+          This site only verifies documents that are MLETR compliant. Please ensure your document adheres to TradeTrust
+          standards before proceeding with the verification process.
+        </div>
+        {/* <Lottie options={defaultOptions} isPaused={false} isClickToPauseDisabled={true} height={400} width={400} /> */}
+      </div>
       {/* <DraggableDemoCertificate chainId={currentChainId} /> */}
     </div>
   ) : (
